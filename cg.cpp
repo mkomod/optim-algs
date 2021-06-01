335601d207c357606d04163f81dc0ba1aa61658e
mkomod <mkomodromos01@gmail.com>
2021-06-01 11:44:36 +0100

    init: cg optim

---
+++ cg.cpp
@@ -0,0 +1,13 @@
+// Conjugate Gradient method
+#include "iostream"
+
+double cg() {
+    // input: function pointer, gradient function pointer
+    return 0.0;
+}
+
+
+int main() 
+{
+    return 0;
+}
